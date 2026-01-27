9ab14b717e991e95a6f5abd90b512a7f5d69b510
richardtcs <richarddavidrv13@gmail.com>
2026-01-27 15:10:59 -0400

    indicador quemado

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 // page.tsx
 'use client';
 
+import { ChartAreaInteractive } from "@/components/indicators/indicator-1";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+
 export default function Page() {
 
     return (
         <div className="container mx-auto py-8 px-4">
-            Indicadores
+            <ChartAreaInteractive />
         </div>
     );
 }

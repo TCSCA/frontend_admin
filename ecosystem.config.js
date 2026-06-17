173272fb7e1974492cd6c67aabd9b5ae3a9a4ccb
simontcs12 <simonjsaadt12@gmail.com>
2026-06-17 11:23:35 -0400

    cambios de ecosystem y next.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   apps: [{
     name: "WebDesa-Administrativo",
-    script: "pnpm",
-    args: "start -p 3001",
-    interpreter: "none",
+    script: ".next/standalone/server.js",
+    cwd: "/var/www/proyectos/InterfazAdministrativa",
+    exec_mode: "fork",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_restarts: 5,
+    restart_delay: 2000,
     env: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      PORT: 3001,
+      HOSTNAME: "0.0.0.0",
+      NEXT_PUBLIC_API_BASE_URL_ASSETS: "ordenesmedicas"
     }
   }]
 }
--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-// next.config.js - Opción B (con trailing slash)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/ordenesmedicas',
-  assetPrefix: '/ordenesmedicas',
-  trailingSlash: false, // ← Si la Opción A falla, prueba con true
+  output: 'standalone',
+  basePath: `/${process.env.NEXT_PUBLIC_API_BASE_URL_ASSETS}`,
+  trailingSlash: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -11,19 +10,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    deviceSizes: [],
-    imageSizes: [],
     unoptimized: true,
-    path: '/ordenesmedicas/_next/image',
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'front.tcs.com.ve',
-        port: '',
-        // ⚠️ IMPORTANTE: Incluye la doble barra '//' como está en tu URL
-        pathname: '/desa_apiMedicamentos/storage/deliveryNotes/**',
-      },
-    ],
   },
 };
 
